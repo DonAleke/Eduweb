96848e8a8a9c895fa995f14e6747303ae797b6b8
johnchumz <johnchumbekwe@gmail.com>
2018-06-05 18:55:48 +0300

    changed git ignore

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use App\Profile;
+use App\subjects;
 
 class RegisterController extends Controller
 {
